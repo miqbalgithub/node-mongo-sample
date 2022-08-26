838e0a579581baa07bd3e78732ede26139938677
miqbalgithub <86734925+miqbalgithub@users.noreply.github.com>
2022-08-26 19:37:43 +0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Configuring the database
-const dbConfig = require('config/database.config.js');
+const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
-require('app/routes/note.routes.js')(app);
+require('./app/routes/note.routes.js')(app);
 
 // listen for requests
 app.listen(8080, () => {
